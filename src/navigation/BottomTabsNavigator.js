90389b998c692450bcbe48f11887e49d77f36bc3
Franco Muzzio <48494284+FrancoMuzzio@users.noreply.github.com>
2023-07-28 16:01:32 -0300

    BottomTabs con iconos y color verde oliva

--- src/navigation/BottomTabsNavigator.js
+++ src/navigation/BottomTabsNavigator.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from '../screens/Home';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Foundation from 'react-native-vector-icons/Foundation';
 import CommunitiesScreen from '../screens/Communities';
 import PlantIdentifierScreen from '../screens/PlantIdentifier';
 import MyPlantsScreen from '../screens/MyPlants';
@@ -11,7 +13,36 @@ const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let IconComponent;
+          let iconName;
+
+          if (route.name === 'My Plants') {
+            iconName = 'flower';
+            IconComponent = MaterialCommunityIcons;
+          } else if (route.name === 'Communities') {
+            iconName = 'account-group';
+            IconComponent = MaterialCommunityIcons;
+          } else if (route.name === 'Plant identifier') {
+            iconName = 'magnifying-glass';
+            IconComponent = Foundation;
+          } else if (route.name === 'Market') {
+            iconName = 'shopping';
+            IconComponent = MaterialCommunityIcons;
+          } else if (route.name === 'Profile') {
+            iconName = 'user';
+            IconComponent = FontAwesome;
+          }
+          return <IconComponent name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'limegreen',
+        inactiveTintColor: 'gray',
+      }}
+    >
       <Tab.Screen name="My Plants" component={MyPlantsScreen} />
       <Tab.Screen name="Communities" component={CommunitiesScreen} />
       <Tab.Screen name="Plant identifier" component={PlantIdentifierScreen} />
